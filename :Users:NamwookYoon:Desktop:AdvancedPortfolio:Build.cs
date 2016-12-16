36b8895ade51b4e2e38ef0c3a300f47296e5a404
how457 <how457@gmail.com>
2016-12-15 20:01:41 -0700

    second commit

---
+++ :Users:NamwookYoon:Desktop:AdvancedPortfolio:Build.cs
@@ -0,0 +1,11 @@
+﻿
+using System;
+namespace CPSC1012_AdvancedPortfolio_NamYoon
+{
+	public class EmptyClassr
+	{
+		public EmptyClassr()
+		{
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CPSC1012_AdvancedPortfolio_NamYoon
+{
+	public class Program
+	{
+		public void Main()
+		{
+			
+		}
+
+		public static int GetNextArrayIndex(ref MultipleChoiceQuestion[] questionArray)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
