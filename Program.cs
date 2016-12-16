14bdf5bff8ae2ce8de6df496099c3ac93abfbbdd
how457 <how457@gmail.com>
2016-12-15 20:02:16 -0700

    3rd commit

--- Program.cs
+++ Program.cs
@@ -4,13 +4,11 @@ namespace CPSC1012_AdvancedPortfolio_NamYoon
 {
 	public class Program
 	{
-		public void Main()
-		{
-			
-		}
 
 		public static int GetNextArrayIndex(ref MultipleChoiceQuestion[] questionArray)
 		{
+			if (1 == 1)
+				return -1;
 			throw new NotImplementedException();
 		}
 	}
