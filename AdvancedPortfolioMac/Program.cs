f0bb488b9963e461cdd9b93d3b16cbda5d99a6c7
how457 <how457@gmail.com>
2016-12-15 20:35:58 -0700

    compatible with mac now

---
+++ AdvancedPortfolioMac/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using CPSC1012_AdvancedPortfolio_NamYoon; //comment this out when running in windows
+
+namespace AdvancedPortfolioMac
+{
+	public class Program
+	{
+		[STAThread]
+		public static void Main()
+		{
+			App myApp = new App();
+			myApp.start();
+		}
+	}
+}
